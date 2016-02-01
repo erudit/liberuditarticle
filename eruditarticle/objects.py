23e78e416861b24dbd8afe112f35a4925fd33761
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-02-01 16:44:42 -0500

    Update empty erudit classes

--- eruditarticle/objects.py
+++ eruditarticle/objects.py
@@ -3,13 +3,22 @@
 from __future__ import unicode_literals
 
 
-class EruditJournal(object):
+class EruditBaseObject(object):
+    def __init__(self, *args, **kwargs):
+        # TODO
+        pass
+
+
+class EruditJournal(EruditBaseObject):
+    # TODO
     pass
 
 
-class EruditPublication(object):
+class EruditPublication(EruditBaseObject):
+    # TODO
     pass
 
 
-class EruditArticle(object):
+class EruditArticle(EruditBaseObject):
+    # TODO
     pass
