79fbebdeb495cfe102159fafe4aacd1b88bf1a3b
David Cormier (Érudit) <david.cormier@erudit.org>
2016-04-19 20:28:16 -0400

    fix pep8

--- eruditarticle/tests/test_journal.py
+++ eruditarticle/tests/test_journal.py
@@ -4,6 +4,7 @@ import pytest
 from eruditarticle.objects import EruditPublication
 from eruditarticle.tests.base import BaseTestCase
 
+
 class TestEruditPublication(BaseTestCase):
 
     @pytest.fixture(autouse=True)
@@ -13,12 +14,9 @@ class TestEruditPublication(BaseTestCase):
         super().setup()
 
     def test_droitauteur(self):
-        assert self.test_objects['images1102374.xml'].get_droitauteur() == "Tous droits réservés © 24 images, 2000"
-        assert self.test_objects['liberte1035607.xml'].get_droitauteur() == "Tous droits réservés © Collectif Liberté, 1993"
+        assert self.test_objects['images1102374.xml'].get_droitauteur() == "Tous droits réservés © 24 images, 2000"  # noqa
+        assert self.test_objects['liberte1035607.xml'].get_droitauteur() == "Tous droits réservés © Collectif Liberté, 1993"  # noqa
 
     def test_droitauteurorg(self):
         assert self.test_objects['images1102374.xml'].get_droitauteurorg() == "24 images"
         assert self.test_objects['liberte1035607.xml'].get_droitauteurorg() == "Collectif Liberté"
-
-    #def test_isbn(self):
-    #    assert self.test_objects['images1102374.xml'].get_isbn() == '2-922607-82-8'
