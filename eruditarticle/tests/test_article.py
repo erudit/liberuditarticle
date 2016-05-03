14d2c049e3c35523b6f3832562804531c9c953cd
Morgan Aubert <morgan.aubert@zoho.com>
2016-05-03 12:50:18 -0400

    Add tests for ISSN / ISSBN

--- eruditarticle/tests/test_article.py
+++ eruditarticle/tests/test_article.py
@@ -15,3 +15,40 @@ class TestArticleSavantComplet(BaseTestCase):
     def test_all_instances(self):
         for object_name, article in self.test_objects.items():
             assert isinstance(article, EruditArticle)
+
+    def test_issn(self):
+        assert self.test_objects['1001948ar.xml'].get_issn() == '0001-771X'
+
+    def test_issn_num(self):
+        assert self.test_objects['1001948ar.xml'].get_issn_num() == '1710-3991'
+
+    def test_isbn(self):
+        assert self.test_objects['1001948ar.xml'].get_isbn() is None
+
+    def test_isbn_num(self):
+        assert self.test_objects['1001948ar.xml'].get_isbn_num() is None
+
+
+class TestArticleCulturelMinimal(BaseTestCase):
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.object_type = EruditArticle
+        self.objects_path = './eruditarticle/tests/fixtures/article/culturel/minimal'
+        super().setup()
+
+    def test_all_instances(self):
+        for object_name, article in self.test_objects.items():
+            assert isinstance(article, EruditArticle)
+
+    def test_issn(self):
+        assert self.test_objects['34598ac.xml'].get_issn() == '0835-7641'
+
+    def test_issn_num(self):
+        assert self.test_objects['34598ac.xml'].get_issn_num() == '1923-3205'
+
+    def test_isbn(self):
+        assert self.test_objects['34598ac.xml'].get_isbn() == '2-922607-82-8'
+
+    def test_isbn_num(self):
+        assert self.test_objects['34598ac.xml'].get_isbn_num() is None
